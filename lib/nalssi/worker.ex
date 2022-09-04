c7a0dd644c8b494862a840d5042ebe12a6426db5
Hyun Ahn <codepawn@icloud.com>
2022-09-05 04:12:02 +0900

    Update worker.ex

--- lib/nalssi/worker.ex
+++ lib/nalssi/worker.ex
@@ -1,14 +1,58 @@
 defmodule Nalssi.Worker do
+  @api_key Application.get_env(:metex, :api_key)
+
+  @absolute_zero -273.15
+
+  def loop do
+    receive do
+      {sender_pid, location} ->
+        send(sender_pid, {:ok, temperature_of(location)})
+
+      _ ->
+        IO.puts("what is this?")
+    end
+    loop
+  end
+
   def temperature_of(location) do
-    result = url_for(location)
+    result = url_for(location) |> HTTPoison.get() |> parse_response
+
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}°C"
+
+      :error ->
+        "#{location} not found"
+    end
   end
 
   defp url_for(location) do
     location = URI.encode(location)
-    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{:api_key}"
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{@api_key}"
   end
 
-  defp parse_response({:ok, %HTTPoinson.Response{body: body, status_code: 200}}) do
-    body |> JSON.decode!
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode!() |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json_body) do
+    try do
+      temp = (json_body["main"]["temp"] + @absolute_zero) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
   end
 end
+
+
+# cities = ["Singapore", "Monaco", "Vatican City", "Hong Kong", "Macau"]
+
+cities |> Enum.each(fn city ->
+  pid = spawn(Nalssi.Worker, :loop, [])
+  send(pid, {self, city})
+end)
